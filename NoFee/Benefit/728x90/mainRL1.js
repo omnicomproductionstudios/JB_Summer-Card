229dfe175b574b00a2e43431aa0ed203d77bff18
Mahesh Bhatt <mahesh.bhatt@omc.com>
2026-04-14 22:11:08 +0530

    sdfd

---
+++ NoFee/Benefit/728x90/mainRL1.js
@@ -0,0 +1,67 @@
+// Banner duration timer start time
+var startTime;
+
+// Timeline references
+var tl;
+var tl1;
+
+function init() {
+  startTime = new Date();
+  
+  // Modern GSAP 3 syntax
+  tl1 = gsap.timeline();
+  tl = gsap.timeline({ onComplete: endTime });
+  
+  animate();
+  setRollover();
+}
+function animate() {
+  tl.set(["#main_content"], { autoAlpha: 1, force3D: true });
+  tl.set('#copy2', {y: 20})
+
+  .addLabel('frame1', 0)
+    .to('#copy1', 1, {autoAlpha: 1, ease: Power1.easeInOut}, 'frame1')
+    .to(['#copy1, #term1'], 0.5, {autoAlpha: 0, ease: Power1.easeInOut}, 'frame1+=3.5')
+    .to('#copy2', 1, {y: 0, autoAlpha: 1, ease: Power1.easeInOut}, 'frame1+=3.7')
+  .addLabel('frame2', 8.5)
+    .to(['#copy2, #card, #logo, #term'], 0.5, {autoAlpha: 0, ease: Power1.easeInOut}, 'frame2')
+    .to('#lastFrame',0.5,{x: 0, ease: Power1.easeInOut}, 'frame2')
+    .to('#shine', 0.5, {backgroundPosition: '535px 0px'}, 'frame2+=1');
+}
+function setRollover() {
+  document
+    .getElementById("default_exit")
+    .addEventListener("mouseover", defaultOver, false);
+  document
+    .getElementById("default_exit")
+    .addEventListener("mouseout", defaultOut, false);
+}
+
+function defaultOver() {
+  TweenMax.to("#cta", 0.15, { scale: 1.1, ease: Power1.easeInOut });
+}
+
+function defaultOut() {
+  TweenMax.to("#cta", 0.15, { scale: 1, ease: Power1.easeInOut });
+}
+
+
+function randomInt(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function endTime() {
+  // show total banner animation time in browser console.
+  var endTimestamp = new Date();
+
+  console.log(
+    "Animation duration: " + (endTimestamp - startTime) / 1000 + " seconds"
+  );
+}
+
+function randomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
